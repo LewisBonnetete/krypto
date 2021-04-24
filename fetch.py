b879c1c827c70d297c303cf488688055c8a37d5a
Lewis <lewis.bonnetete@gmail.com>
2021-04-24 03:47:52 +0200

    writing on fleek

--- fetch.py
+++ fetch.py
@@ -8,17 +8,22 @@ proxy = "https://secret-refuge-22081.herokuapp.com/"
 
 response = requests.get(url+"coin/latest?coin=BTC&convert=BTC,ETH,USD")
 
-def jsonPrint(obj):
+def json_print(obj):
     # create a formatted string of the Python JSON object
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
+
+def json_write(obj):
+    # Stores the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
     with open("infos.json", "w") as outfile:
         outfile.write(text)
 
-print("\n\n")
+print("\n")
 
 
-jsonPrint(response.json())
+# json_print(response.json())
+json_write(response.json())
 
 
-print("\n\n")
+print("\n")
--- get_crypto_infos.py
+++ get_crypto_infos.py
@@ -4,7 +4,6 @@ def get_crypto_infos(crypto_name, info_name, date)
 	# crypto_name = Name of the crypto currency you want info about
 	# info_name = Name of the info you want
 	# date = Date of the info
-
 	# Ex: get_crypto_infos("BTC", "USD", "latest")
 	# Returns 56098.94535250209
 	
